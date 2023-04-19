ef6f20df8aa43326741ff9e0f19a4da9350c3d71
Paolo96700 <paolo.falcoapp@gmail.com>
2023-04-19 14:57:56 +0200

    Esercizio

---
+++ script.js
@@ -0,0 +1,21 @@
+const arrList = [
+    'Latte',
+    'Pomodori',
+    'Acqua',
+    'Caffè',
+    'Peperoni',
+    'Pasta',
+    'SALMONE',
+    'Uova',
+];
+
+const eleList = document.querySelector('#lista');
+let i = 0
+
+
+while ( i < arrList.length) {
+    console.log(arrList[i]);
+    eleList.innerHTML += `<li>${arrList[i]}</li>`;
+    i++;
+}
+
