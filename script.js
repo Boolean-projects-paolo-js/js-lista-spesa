8d4ada2a453ff92c37ef82427ba7b784c2dd7423
Paolo Falco <paolo.falcoapp@gmail.com>
2024-02-13 19:29:52 +0100

    Fix repo with script

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let arrayList = document.querySelector('#lista');
+
+
 const arrList = [
     'Latte',
     'Pomodori',
@@ -9,13 +12,33 @@ const arrList = [
     'Uova',
 ];
 
-const eleList = document.querySelector('#lista');
-let i = 0
-
-
-while (i < arrList.length) {
-    console.log(arrList[i]);
-    eleList.innerHTML += `<li>${arrList[i]}</li>`;
-    i++;
+function createList(array){
+    arrayLength = array.length;
+    // Pulisce il contenuto della lista
+    arrayList.innerHTML = '';
+    for (let i = 0; i < arrayLength; i++) {
+        //crea un elemento <li></li>
+        let listItem = document.createElement('li');
+        //aggiunge nel tag gli elementi degli array
+        listItem.textContent = array[i];
+        //aggiunge l'elemento <li> (contenente il testo specificato)
+        arrayList.appendChild(listItem);
+    }
 }
+
+function addToList(array){ 
+    let inputList = document.querySelector('#input');
+    let buttonList = document.querySelector('button');
+
+    buttonList.addEventListener('click', function(){
+        array.push(inputList.value);
+        console.log(array);
+        createList(array);
+    });
+}
+
+
+createList(arrList);
+addToList(arrList);
+
 
